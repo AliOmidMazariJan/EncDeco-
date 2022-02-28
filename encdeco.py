31d79f540f482a018e9d74766ba8d422a67ae59f
Ali Omid Mazari <99080700+AliOmidMazariJan@users.noreply.github.com>
2022-02-28 22:22:53 +0430

    encdeco.py
    https://github.com/AliOmidMazariJan
    ls
    cd EncDeco
    ls
    python2 encdeco.py

---
+++ encdeco.py
@@ -0,0 +1,105 @@
+# SH-BD THIS TOOLS ARE MAKE BY FREEðŸ˜„
+
+<p align="center">
+
+<a href="https://github.com/Dark-Cyber-07"><img title="D A R K C Y B E R " src="https://github-readme-stats.vercel.app/api?username=Dark-Cyber-07&show_icons=true&include_all_commits=true&theme=chartreuse-dark&cache_seconds=3200"></a>
+
+</p>
+
+<p align="center">
+
+<a href="#"><img title="succeed" src="https://img.shields.io/badge/deobfuscating-succeed-green?colorB=%23017e40&style=for-the-badge"></a>
+
+</p>
+
+<br/><br/>
+
+<h1 align="center">SH-BDV1</h1>
+
+<p align="center">      ACCOUNT CLONER FOR TERMUX USERS</p>
+
+## ***About SH-BDV1***:
+
+BD-11 is a python based cloning script. You can clone unlimited Bangladeshi facebook accounts by using this tool. This tool works on any Android devices without root.
+
+## Install on Termux :
+
+```
+
+pkg update && pkg upgrade
+
+pkg install python
+
+pkg install python2
+
+pkg install git
+
+pkg install pip
+
+pkg install pip2
+
+pip2 install requests
+
+pip2 install mechanize
+
+github.com/AliOmidMazariJan/EncDeco-/new/main
+
+```
+
+## Install on Terminal :
+
+```
+
+apt update && apt upgrade
+
+apt install python
+
+apt install python2
+
+apt install git
+
+apt install pip
+
+apt install pip2
+
+pip2 install requests
+
+pip2 install mechanize
+
+github.com/AliOmidMazariJan/EncDeco-/new/main
+
+```
+
+## Tools Run :
+
+```
+
+ls && cd SH-BD && ls
+
+python2 sh-all.py 
+
+```
+
+7-12 DIGIT CLONE USER NAME OR PASSWORD SAME
+
+*   USERNAME: LoveYou
+
+*   Passwords : DARKCYBER
+
+ONLY 6 DIGIT PASSWORD ARE DIFFERENT 
+
+USER NAME : DarkCyber
+
+Passwords : SHaNtO
+
+## ***Follow Me***
+
+* Telegram : [Join Now](https://t.me/RaYeesHcker)
+
+* Facebook  : [Follow Me](https://www.facebook.com/RayeesOmidMazari555)
+
+* GitHub : [Follow Me](https://github.com/AliOmidMazariJan/EncDeco-/new/main)
+
+### Warning
+
+***Don't try to edit or modify this tool. This tool is only for educational purpose. If you use this tool for other purposes except education we will not be responsible in such cases.***
